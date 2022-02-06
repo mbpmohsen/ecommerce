c56a8e495bdd58181f863b0274990ddc3f00c6a9
mbpmo <mohsen.babaei@podro.com>
2022-02-06 15:27:35 +0330

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+const random = Math.random() > 0.5 ? "Hello" : [1, 2];
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
